f753aed478233390d3f9f135cfb4a7f63d488f9e
Aleh Shchurko <alehshchurko@gmail.com>
2022-02-17 15:41:25 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import styles from "./style.css";
+let main = document.querySelector('.main');
+let container = document.createElement('div');
+container.classList.add('container');
+main.append(container);
+let a = 'dgdfg'
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread']
+              }
+            }
+          }
+        ],
+    },
+}
