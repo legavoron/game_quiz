ed40c300cc8b1bdf77baae3768f96f2f18783d9b
Aleh Shchurko <alehshchurko@gmail.com>
2022-02-18 21:37:29 +0300

    add functions game and check answer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import styles from "./style.css";
 import Star from './star.svg';
 
+// ----------------------------------------------------- Layout ---------------------------------------------------
 
 function createfonImages() {
     let wrapper = document.createElement('div');
@@ -43,13 +44,10 @@ let flagBox = document.querySelector('.flag__box');
 function createFlag() {
     let flag = document.createElement('img');
     flag.classList.add('flag');
-    flag.alt = 'flag';
     flagBox.append(flag);
 }
 createFlag();
 let flag = document.querySelector('.flag');
-
-flag.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Webpack.svg/1200px-Webpack.svg.png';
 
 function createBtnsContainer() {
     let btnsContainer = document.createElement('div');
@@ -59,18 +57,41 @@ function createBtnsContainer() {
 createBtnsContainer();
 let btnsContainer = document.querySelector('.btns__container');
 
-function createBtn(id, value) {
+function createBtn(id) {
     let btn = document.createElement('a');
     btn.classList.add('btn');
     btn.id = id;
-    btn.innerText = value;
     
     btnsContainer.append(btn);
 }
-createBtn('btn1', 'Russia');
-createBtn('btn2', 'France');
-createBtn('btn3', 'China');
-createBtn('btn4', 'Kazachstan');
+createBtn('btn1');
+createBtn('btn2');
+createBtn('btn3');
+createBtn('btn4');
+
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+let btns = document.querySelectorAll('.btn');
+
+function createLandInfoBox() {
+    let landInfoBox = document.createElement('div');
+    landInfoBox.classList.add('land__info__box');
+    btnsContainer.append(landInfoBox);
+}
+createLandInfoBox();
+let landInfoBox = document.querySelector('.land__info__box');
+
+function createLandInfoBoxContainer(id) {
+    let landInfoBoxContainer = document.createElement('span');
+    landInfoBoxContainer.classList.add('land_info_box_container');
+    landInfoBoxContainer.id = id;
+    landInfoBox.append(landInfoBoxContainer);
+}
+createLandInfoBoxContainer('landInfoBoxContainerName');
+createLandInfoBoxContainer('landInfoBoxContainerCountri');
+createLandInfoBoxContainer('landInfoBoxContainerPopulation');
 
 function createScoreContainer() {
     let scoreContainer = document.createElement('div');
@@ -81,7 +102,6 @@ createScoreContainer();
 let scoreContainer = document.querySelector('.score__container'); 
 
 function createStarImg() {
-    // let star = document.createElement('img');
     let star = new Image();
     star.classList.add('star');
     star.alt = 'star';
@@ -100,7 +120,6 @@ function createPoints() {
     totalPoints.classList.add('player__Points');
     totalPoints.innerText = '/ 20'
     scoreContainer.append(totalPoints);
-
 }
 createPoints();
 let playerPoints = document.querySelector('#playerPoints');
@@ -113,4 +132,189 @@ function createMessage() {
 }
 createMessage();
 let message = document.querySelector('.message');
-message.innerText = 'WRONG';
+
+//------------------------------------------------------ Game -------------------------------------------------------------
+let landResult = '';
+let randomLand1 = '';
+let randomLand2 = '';
+let randomLand3 = '';
+
+game();
+
+function game() {
+
+    hiddenInfoBox();
+    wrapper.removeEventListener('click', startNewGame);
+    
+    let fetchInfo = fetch('https://api.sampleapis.com/countries/countries')
+    .then(response => response.json())
+    .then(json => {
+        
+        let numLandResult = '';
+        let numLandRandom1 = '';
+        let numLandRandom2 = '';
+        let numLandRandom3 = '';
+
+        searchLands();
+
+        function searchLands() {
+            numLandResult = getRandomIntInclusive(1, json.length);
+            numLandRandom1 = getRandomIntInclusive(1, json.length);
+            numLandRandom2 = getRandomIntInclusive(1, json.length);
+            numLandRandom3 = getRandomIntInclusive(1, json.length);
+
+            if (numLandResult === numLandRandom1 || numLandResult === numLandRandom2 || numLandResult === numLandRandom3) {
+                searchLands();
+            }
+            if (numLandRandom1 === numLandRandom2 || numLandRandom1 === numLandRandom3) {
+                searchLands();
+            }
+            if (numLandRandom2 === numLandRandom3) {
+                searchLands();
+            }
+        }
+
+        landResult = json[numLandResult];
+        randomLand1 = json[numLandRandom1];
+        randomLand2 = json[numLandRandom2];
+        randomLand3 = json[numLandRandom3];
+        
+        if(landResult.media.flag) {
+            flag.src = landResult.media.flag;
+        } else {
+            game();
+        }
+        
+        setBtnsValues()
+
+        function setBtnsValues() {
+            let arr = [btn1, btn2, btn3, btn4];
+            let random = getRandomIntInclusive(0, 3);
+            arr[random].innerHTML = landResult.name;
+            arr.splice(random, 1);
+            arr[0].innerHTML = randomLand1.name;
+            arr[1].innerHTML = randomLand2.name;
+            arr[2].innerHTML = randomLand3.name;
+        }
+
+        btns.forEach(elem => {
+            elem.addEventListener('click', checkAnswer);
+        });
+    })
+}
+
+
+function checkAnswer() {
+    if (this.innerHTML === landResult.name) {
+
+        btns.forEach(elem => {
+            elem.removeEventListener('click', checkAnswer);
+        });
+        
+        this.style.backgroundColor = '#02705C';
+        setTimeout(()=> {
+            message.style.backgroundColor = '#02705C';
+            message.innerHTML = 'Right';
+            message.style.display = 'flex';
+
+            setTimeout(()=> {
+                message.style.display = 'none';
+                showLandsInfo();
+            },1000);
+        },500);
+    } else {
+        btns.forEach(elem => {
+            elem.removeEventListener('click', checkAnswer);
+        });
+
+        this.style.backgroundColor = '#A30312';
+        
+        btns.forEach(elem => {
+            if (elem.innerHTML === landResult.name) {
+                elem.style.backgroundColor = '#02705C';
+            }
+        });
+
+        setTimeout(()=> {
+            message.style.backgroundColor = '#A30312';
+            message.innerHTML = 'Wrong';
+            message.style.display = 'flex';
+
+            setTimeout(()=> {
+                message.style.display = 'none';
+                showLandsInfo();
+            },1000);
+        },500);
+    }
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
+function showLandsInfo() {
+    showInfoBox();
+    btns.forEach(elem => {
+        elem.style.display = 'none';
+    });
+    
+    let infoName = document.querySelector('#landInfoBoxContainerName');
+    let infoCountry = document.querySelector('#landInfoBoxContainerCountri');
+    let infoPopulation = document.querySelector('#landInfoBoxContainerPopulation');
+
+    infoName.innerHTML = `Land: ${landResult.name}`;
+
+    if (landResult.capital) {
+        infoCountry.innerHTML = `Capital: ${landResult.capital}`;
+    }
+    if (landResult.population) {
+        let num = editNumPeople();
+        infoPopulation.innerHTML = `Population: ${num} people`;
+    }
+    wrapper.addEventListener('click', startNewGame);
+}
+function editNumPeople() {
+    let num = landResult.population;
+    num += "";
+    num = num.split('');
+    if (num.length > 3) {
+        num.splice(-3, 0, " ");
+    }
+    if (num.length > 7) {
+        num.splice(-7, 0, " ");
+    }
+    if (num.length > 10) {
+        num.splice(-10, 0, " ");
+    }
+    if (num.length > 13) {
+        num.splice(-13, 0, " ");
+    }
+    return num.join('');
+}
+
+function showInfoBox() {
+    btnsContainer.style.backgroundColor = '#BDD4F2';
+    btnsContainer.style.borderRadius = '20px';
+    btnsContainer.style.border = '10px solid white';
+    btnsContainer.style.boxSizing = "border-box";
+    landInfoBox.style.display = 'flex';
+}
+
+function hiddenInfoBox() {
+    btnsContainer.style.backgroundColor = 'none';
+    btnsContainer.style.borderRadius = '20px';
+    btnsContainer.style.border = 'none';
+    btnsContainer.style.boxSizing = "content-box";
+    landInfoBox.style.display = 'none';
+
+    btns.forEach(elem => {
+        elem.style.display = 'flex';
+        elem.style.backgroundColor = '#4F5C88';
+    });
+}
+function startNewGame() {
+    hiddenInfoBox();
+    game();
+}
